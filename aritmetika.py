11a3642922d61045eac70f47c27df0b105b0295c
kakdidi <kakdidi@gmail.com>
2021-02-25 05:31:32 +0000

    menambahkan file aritmetika

---
+++ aritmetika.py
@@ -0,0 +1,30 @@
+#operasi aritmetika
+a = 10
+b = 3
+hasil = a + b
+#operasi tambah +
+print(a, " + ", b, " = ", hasil)
+
+#operasi kurang -
+hasil = a - b
+print(a, " - ", b, " = ", hasil)
+
+#operasi kali
+hasil = a * b
+print(a, " * ", b, " = ", hasil)
+
+#operasi bagi
+hasil = a / b
+print(a, " / ", b, " = ", hasil)
+
+#operasi eksponen
+hasil = a ** b
+print(a, " ** ", b, " = ", hasil)
+
+#operasi modulus
+hasil = a % b
+print(a, " % ", b, " = ", hasil)
+
+#operasi floor division (lawan modulus)
+hasil = a // b
+print(a, " // ", b, " = ", hasil)
