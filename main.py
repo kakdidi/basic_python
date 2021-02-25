5cee04b6535db091933497bf7237ced0ed138c14
kakdidi <kakdidi@gmail.com>
2021-02-25 02:11:51 +0000

    menambahkan main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#kita belajar casting
+#merubah dari satu tipe ke tipe yang lain
+#tipe data int, float, str, bool
+
+data_int = 9
+print("data ", data_int, ",type = ", type(data_int))
+
+data_float = float(data_int)
+print("data ", data_int, ",type = ", type(data_float))
+data_string = str(data_int)
+data_boolean = bool(data_int)
+print("data ", data_string, ",type = ", type(data_string))
+print("data ", data_boolean, ",type = ", type(data_boolean))
+
